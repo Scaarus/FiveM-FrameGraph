d491e2a85cec617e0e3c3eb40449cc54b9492bf8
Scaarus <michael@scaarus.com>
2022-06-17 00:01:57 -0400

    Add command for toggling the graph overlay

--- client.lua
+++ client.lua
@@ -11,3 +11,41 @@ Citizen.CreateThread(function()
         Index = Index + 1
     end
 end)
+
+TriggerEvent('chat:addSuggestion', '/fpsavg', 'Average FPS over the given duration', {
+    { name = "duration", help = "How long to record FPS in seconds" }
+})
+
+RegisterCommand("fpsavg", function(_, args, _)
+    local duration = tonumber(args[1])
+    if duration == nil then
+        TriggerEvent('chat:addMessage', {
+            color = { 255, 0, 0},
+            multiline = false,
+            args = { "FrameGraph", "`"..args[1].."` is not a valid number."}
+        })
+        return
+    end
+
+    TriggerEvent('chat:addMessage', {
+        color = { 255, 0, 0},
+        multiline = false,
+        args = { "FrameGraph", "Measuring FPS for "..duration.." seconds."}
+    })
+
+    local frameCount = GetFrameCount()
+    Citizen.Wait(tonumber(args[1]) * 1000)
+    local average = (GetFrameCount() - frameCount) / duration
+
+    TriggerEvent('chat:addMessage', {
+        color = { 255, 0, 0},
+        multiline = false,
+        args = { "FrameGraph", "Average FPS over "..duration.." seconds: "..average}
+    })
+end, false)
+
+TriggerEvent('chat:addSuggestion', '/fps', 'Toggle the FPS graph', {})
+
+RegisterCommand("fps", function(_, _, _)
+    SendNUIMessage({ type = "toggle" })
+end, false)
